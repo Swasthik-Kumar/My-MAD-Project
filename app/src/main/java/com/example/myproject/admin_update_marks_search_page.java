aa547d7a925f313d83eda4fe9a18494549e51337
Swasthik Kumar <swasthikkumarkombettu@gmail.com>
2022-06-24 22:19:08 +0530

    My Project

---
+++ app/src/main/java/com/example/myproject/admin_update_marks_search_page.java
@@ -0,0 +1,39 @@
+package com.example.myproject;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class admin_update_marks_search_page extends AppCompatActivity{
+    TextView search;
+    ArrayList arrayList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_update_marks_search_page);
+        search = findViewById(R.id.admin_search_student);
+
+        String username_got = getIntent().getStringExtra("username");
+        database_handler admin_student_search_handler = new database_handler(this, "My_Project.db",null,1);
+        arrayList = (ArrayList) admin_student_search_handler.get_student_info(username_got);
+
+    }
+
+
+ /*       int count = arrayList.size();
+//        Toast.makeText(this, ""+count, Toast.LENGTH_LONG).show();
+        array = arrayList.get(0);
+//        Toast.makeText(this, ""+arrayList.get(0), Toast.LENGTH_LONG).show();
+
+        for(int i=0; i<count*4;i+=4){
+            arrayList_display.add(new landlord_search_pattern(R.drawable.ic_launcher_background,""+array.get(i),""+array.get(i+1)+"  Rs."+array.get(i+2),""+array.get(i+3)));
+
+        }
+
+        custom_adapter customadapter = new custom_adapter(this, R.layout.landlord_search_page_pattern,arrayList_display);
+        listView.setAdapter((customadapter));*/
+
+    }
